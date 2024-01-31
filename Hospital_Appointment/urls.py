d598300dfcaeba6a033296d912c174466e138ebe
Getonto3 <getontomoses18@gmail.com>
2024-01-31 13:47:17 +0300

    feat: Hospital_appointment app urls

--- Hospital_Appointment/urls.py
+++ Hospital_Appointment/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 
-from .views import DepartmentList, DoctorDetails
+from .views import (
+    DepartmentList,
+    DoctorDetails,
+    UserRegistration,
+    PatientDetails,
+    AppointmentsDetails,
+)
 
 urlpatterns = [
-    path('department/', DepartmentList.as_view()),
-    path('doctor/', DoctorDetails.as_view()),
+    path("department/", DepartmentList.as_view()),
+    path("doctor/", DoctorDetails.as_view()),
+    path("user/", UserRegistration.as_view()),
+    path("patient/", PatientDetails.as_view()),
+    path("appointments/", AppointmentsDetails.as_view()),
 ]
