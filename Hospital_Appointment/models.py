64ec1b9709d3def7bff7ce2f87df7acda2029ff6
Getonto3 <getontomoses18@gmail.com>
2024-01-31 13:42:00 +0300

    feat: models and migrations for Patient details and appointments

--- Hospital_Appointment/models.py
+++ Hospital_Appointment/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Department(models.Model):
     DepartmentName = models.CharField(max_length=50)
     DepartmentID = models.CharField(max_length=15)
@@ -7,13 +8,44 @@ class Department(models.Model):
     def __str__(self):
         return self.DepartmentName
 
+
 class Doctor(models.Model):
     FirstName = models.CharField(max_length=50)
     LastName = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50,unique=True)
+    email = models.EmailField(max_length=50, unique=True)
     PhoneNumber = models.IntegerField()
-    StaffID = models.CharField(max_length=15,unique=True)
+    Doctor_StaffID = models.CharField(max_length=15, unique=True)
     Department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.FirstName
+        return self.Doctor_StaffID
+
+
+class Patient(models.Model):
+    FirstName = models.CharField(max_length=50)
+    LastName = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    PhoneNumber = models.IntegerField()
+    Patient_IDNumber = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.Patient_IDNumber
+
+
+class Appointments(models.Model):
+    Patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    Doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    Department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    AppointmentDate = models.DateField()
+    AppointmentTime = models.TimeField()
+
+    def __str__(self):
+        return (
+            str(self.Patient)
+            + " "
+            + str(self.Doctor)
+            + " "
+            + str(self.AppointmentDate)
+            + " "
+            + str(self.AppointmentTime)
+        )
