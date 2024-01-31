a8e70eb2e5b9d1f4859905a9a0a8b0c38f306342
Getonto3 <getontomoses18@gmail.com>
2024-01-31 13:43:55 +0300

    feat: views for User registration,patient details and appointments

--- Hospital_Appointment/views.py
+++ Hospital_Appointment/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
 from rest_framework import generics
-from .serializers import DepartmentSerializer,DoctorSerializer
-from .models import Department,Appointment, Doctor, User
+from django.contrib.auth.models import User
+
+from .serializers import (
+    DepartmentSerializer,
+    DoctorSerializer,
+    UserSerializer,
+    PatientSerializer,
+    AppointmentsSerializer,
+)
+from .models import Department, Doctor, Patient, Appointments
 
 
 class DepartmentList(generics.ListCreateAPIView):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
-    
+
+
 class DoctorDetails(generics.ListCreateAPIView):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
+
+
+class PatientDetails(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+
+class UserRegistration(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class AppointmentsDetails(generics.ListCreateAPIView):
+    queryset = Appointments.objects.all()
+    serializer_class = AppointmentsSerializer
