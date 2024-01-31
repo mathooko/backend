e665b7a9923c1192627909dfee579b71fa893655
Getonto3 <getontomoses18@gmail.com>
2024-01-31 13:43:17 +0300

    feat: serializers for User registration,patient details and appointments

--- Hospital_Appointment/serializers.py
+++ Hospital_Appointment/serializers.py
@@ -1,13 +1,34 @@
 from rest_framework import serializers
-from .models import Appointment, Doctor, Department, User
+from django.contrib.auth.models import User
+
+from .models import Doctor, Department, Patient, Appointments
+
 
 class DepartmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Department
-        fields = "__all__"
+        fields = ("DepartmentName", "DepartmentID")
 
 
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
+        fields = "__all__"
+
+
+class PatientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Patient
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("email", "password")
+
+
+class AppointmentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointments
         fields = "__all__"
