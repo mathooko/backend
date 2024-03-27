4245c2f15bbb67ee6226eabfa3b10ccde8b7073e
waribuu <josh200367@gmail.com>
2024-03-27 10:02:23 +0300

    feat: services url

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import CustomerListView
+from .views import CustomerListView, CustomerLoginView,LaundryServiceView
 
 urlpatterns=[
     path("signup/",CustomerListView.as_view(), name="customer_signup"),
-    
-    
+    path("login/",CustomerLoginView.as_view(), name= "customer_login"),
+    path("services/",LaundryServiceView.as_view(),name="services")       
 ]
