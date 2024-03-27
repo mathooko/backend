3f58b47885b3dba536e989d2c3d40c0c1a91a00d
waribuu <josh200367@gmail.com>
2024-03-27 10:03:14 +0300

    feat: services model & migrations

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class LaundryService(models.Model):
+    DRY_CLEANING = 'Drycleaning'
+    DRY_CLEANING_AND_IRONING = 'Drycleaning and ironing'
+    HOUSE_CLEANING = 'House cleaning'
+    SHOE_LAUNDRY = 'Shoe laundry'
+
+    SERVICE_CHOICES = [
+        (DRY_CLEANING, 'Drycleaning'),
+        (DRY_CLEANING_AND_IRONING, 'Drycleaning and ironing'),
+        (HOUSE_CLEANING, 'House cleaning'),
+        (SHOE_LAUNDRY, 'Shoe laundry'),
+    ]
+
+    service = models.CharField(max_length=100, blank=True)
